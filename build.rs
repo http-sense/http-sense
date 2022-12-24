71961f36dc1d568f0db602cd03a1dfbdb6b98d4b
Ankit Saini <nnkitsaini@gmail.com>
2022-12-24 09:32:45 +0530

    test: build

--- build.rs
+++ build.rs
@@ -6,13 +6,16 @@ fn main() {
     // Tell Cargo that if the given file changes, to rerun this build script.
     println!("cargo:rerun-if-changed=frontend/");
     // Use the `cc` crate to build a C file and statically link it.
-    //let output = if cfg!(target_os = "windows") {
+    // let output = if cfg!(target_os = "windows") {
 
     let _output = Command::new("bash")
-            .args(["-c", "false && cd frontend && npm i && npm run build"])
+            .args(["-c", "cd frontend && npm i && npm run build"])
             .output()
             .expect("failed to execute process");
-    //panic!("run? {:?}", output);
+
+    if !_output.status.success() {
+        panic!("couldn't run command successfully:  {:?}", _output);
+    }
     //} else {
     //    Command::new("sh")
     //            .arg("-c")
